79d82d232ee105b743367547c92f75dadba646da
galthran-wq <levmorozov900@gmail.com>
2024-11-03 18:57:07 +0300

    train handler

--- src/lib/web/handlers.py
+++ src/lib/web/handlers.py
@@ -4,6 +4,11 @@ from src.lib.models import MODELS_MAP
 from src.lib.trainer import Trainer, Metrics
 from src.lib.datasets.interfaces import Dataset
 from src.lib.models.interfaces import ModelInterface, ModelConfig
+from src.lib.datasets.interfaces import Data
+from src.lib.web.interfaces import PredictRequest
+from src.lib.storage.local_artifact_storage import LocalArtifactStorage
+
+storage = LocalArtifactStorage()
 
 
 def train_handler(request: TrainRequest) -> dict:
@@ -11,7 +16,10 @@ def train_handler(request: TrainRequest) -> dict:
     test_dataset: Dataset | None = DATASETS_MAP[request.dataset.name].load(split="test")
     model: ModelInterface = MODELS_MAP[request.model.name](request.model.configuration)
     trainer = Trainer(model, train_dataset, test_dataset)
-    metrics: Metrics = trainer.fit()
+    model: ModelInterface = trainer.fit()
+    metrics = trainer.evaluate()
+    artifact_name = storage.save(model, request.dataset.name)
     return {
+        "artifact_name": artifact_name,
         "metrics": metrics.model_dump()
     }
