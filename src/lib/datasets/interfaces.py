2b9818fe466a3277241310e6faab492a03f7f461
Khaymon <khaymonenko@gmail.com>
2024-11-03 13:54:01 +0300

    Add datasets interfaces

---
+++ src/lib/datasets/interfaces.py
@@ -0,0 +1,44 @@
+import abc
+from copy import deepcopy
+import typing as T
+
+import pandas as pd
+
+
+class Dataset(abc.ABC):
+    def __init__(self, texts: T.Sequence[str]):
+        self._texts = list(texts)
+
+    def to_list(self) -> T.List[str]:
+        return deepcopy(self._texts)
+
+    def to_pandas(self) -> pd.Series:
+        return pd.Series(self._texts, name="text")
+
+
+class Targets(abc.ABC):
+    def __init__(self, targets: T.Sequence[int]):
+        self._targets = targets
+
+    def to_list(self) -> T.List[str]:
+        return deepcopy(self._targets)
+    
+    def to_pandas(self) -> pd.Series:
+        return pd.Series(self._targets, name="target")
+
+
+class TrainDataset(abc.ABC):
+    def __init__(self, dataset: Dataset, targets: Targets):
+        self._dataset = dataset
+        self._targets = targets
+
+    @property
+    def data(self):
+        return deepcopy(self._dataset)
+
+    @property
+    def targets(self):
+        return deepcopy(self._targets)
+
+    def to_pandas(self) -> pd.DataFrame:
+        return pd.concat([self._dataset.to_pandas(), self._targets.to_pandas()])
